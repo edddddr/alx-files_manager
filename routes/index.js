b8607393c83acec919d19e113227d5d2e83d9cba
edddddr <endt6342@gmail.com>
2023-12-04 03:53:35 -0800

    0 js

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+import AppController from "../controllers/AppController";
+import UsersController from '../controllers/';
+import AuthController from '../controllers/AuthController.js';
+
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/status', AppController.getStatus);
+router.get('/stats', AppController.getStats);
+router.get('/',(_, res) => {
+  res.send('Home Page');
+ });
+router.post('/users', UsersController.postNew);
+route.get('/users/me', UsersController.getMe);
+
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+
+export default router
